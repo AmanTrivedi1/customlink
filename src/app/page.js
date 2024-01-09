077a3c66ab8581fe6a48067fc6ca2d456429a97d
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-01-09 23:23:00 +0530

    done homepage

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ import { SocialIcon } from 'react-social-icons'
 export default function Home() {
   return (
     <>
-   <main className="bg-primmary md:h-screen h-full">
+   <main className="bg-primmary px-4 md:px-4 lg:px-2 xl:p-0 lg:h-screen h-full ">
        <Header/>
-     <section className=" md:px-0 px-4 2xl:max-w-7xl max-w-6xl m-auto flex md:flex-row flex-col-reverse justify-between">
+     <section className="  2xl:max-w-7xl 2xl:mt-4 mt-0 max-w-6xl m-auto flex md:flex-row flex-col-reverse justify-between">
       <div className="max-w-lg md:mt-20 mt-0">
         <h1 className=" text-4xl md:text-5xl text-secondry lg:text-6xl font-semibold">Everything you are In one, simple link in bio.</h1>
           <p className=" md:mt-8   text-base text-secondry sm:text-xl">Join 40M+ people using Linktree for their link in bio. One link to help you share everything you
@@ -22,11 +22,16 @@ export default function Home() {
               <SocialIcon style={{width:40, height:40}} network="linkedin" />
               </div>
               <div>
-              <h1>Everything in one place</h1>
+              <h1> in one place</h1>
               </div>
             </div>
-          
-        </div>
+          </div>
+          <div>
+            <form className="flex mt-10 items-start " >
+                <input  className="max-w-sm py-2 focus:outline-none focus:shadowsecondry" placeholder="username..."  type="text"/>
+                <button type="submit" className="bg-black  text-white rounded-sm px-6 py-2 ">Joinnow</button>
+            </form>
+          </div>
       </div>
         <div>
         <Image src="/herobg.svg" width={600}  height={400} />               
