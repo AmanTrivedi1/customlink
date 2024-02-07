0ca25071a4023dbb652299c99cde94f6e9aaf6a1
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-02-07 19:42:50 +0530

    final touch

--- src/app/(default)/VisualPage.js
+++ src/app/(default)/VisualPage.js
@@ -5,13 +5,17 @@ import React from 'react'
 import { CiLink } from "react-icons/ci";
 const VisualPage = () => {
   return (
-    <AnimationWrapper>
+   
     <div className='h-full  pt-12 pb-12 bg-normal-dark text-white'>
      <div className='max-w-7xl p-4 m-auto'>
       <div>
+        
         <div className='flex  mt-10 items-center justify-center'>
-             <h1 className='border flex  items-center gap-x-2 border-white/30  py-2 px-4 rounded-full'> <CiLink/>Welcome to Linkit! </h1>
+             <AnimationWrapper>
+                <h1 className='border flex  items-center gap-x-2 border-white/30  py-2 px-4 rounded-full'> <CiLink/>Welcome to Linkit! </h1>
+             </AnimationWrapper>
         </div>
+        <AnimationWrapper>
          <div className='flex flex-col mt-8 items-center justify-center'>
            <h1 className='xl:text-7xl md:text-5xl font-semibold text-5xl' >Link It</h1>
             <p className='md:text-base textsm text-white/80'>Paste everylink that you want to track</p>
@@ -25,10 +29,11 @@ const VisualPage = () => {
            </Link>
         </div>
         </div>
+        </AnimationWrapper>
       </div>
      </div>
     </div>
-    </AnimationWrapper>
+  
   )
 }
 
