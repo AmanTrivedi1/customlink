d606b978ccd21a10150e5dccf355c4a3b0d35174
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-02-07 19:38:40 +0530

    ffffdds

--- src/app/(default)/VisualPage.js
+++ src/app/(default)/VisualPage.js
@@ -1,37 +1,34 @@
 
+import AnimationWrapper from '@/components/Animation';
 import Link from 'next/link';
 import React from 'react'
 import { CiLink } from "react-icons/ci";
 const VisualPage = () => {
   return (
+    <AnimationWrapper>
     <div className='h-full  pt-12 pb-12 bg-normal-dark text-white'>
      <div className='max-w-7xl p-4 m-auto'>
       <div>
         <div className='flex  mt-10 items-center justify-center'>
-        
              <h1 className='border flex  items-center gap-x-2 border-white/30  py-2 px-4 rounded-full'> <CiLink/>Welcome to Linkit! </h1>
-         
-         
         </div>
-       
          <div className='flex flex-col mt-8 items-center justify-center'>
            <h1 className='xl:text-7xl md:text-5xl font-semibold text-5xl' >Link It</h1>
             <p className='md:text-base textsm text-white/80'>Paste everylink that you want to track</p>
-            <p className='text-center max-w-md text-sm mt-10'>Welcome to Linkit! Our platform offers a seamless solution for managing and sharing all your
-               social media and important links in one convenient location. With Linkit, you can effortlessly
-                consolidate your online presence and track views on your shared links in real-time, gaining 
-                valuable insights into your reach and engagement. Whether you're an influencer, business owner,
-                 or individual looking to streamline your online presence.</p>
+            <p className='text-center max-w-md text-sm mt-10'>Welcome to Linkit Our platform offers a seamless solution for managing and sharing all your
+              social media and important links in one convenient location. With Linkit, you can effortlessly
+              consolidate your online presence and track views on your shared links in real-time and gaining 
+              valuable insights into your reach and engagement.</p>
          <div>
-          <Link href="/login">
+           <Link href="/login">
              <button className='bg-white text-normal-dark mt-10 rounded-lg ng-white py-2 px-12'>Signup</button>
            </Link>
         </div>
         </div>
-       
       </div>
      </div>
     </div>
+    </AnimationWrapper>
   )
 }
 
