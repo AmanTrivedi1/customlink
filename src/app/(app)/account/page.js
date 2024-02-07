7e1bacc42468d6ddc930047e90848c93104f1a16
AmanTrivedi1 <amantrivedi598@gmail.com>
2024-02-07 20:24:23 +0530

    fixing errors

--- src/app/(app)/account/page.js
+++ src/app/(app)/account/page.js
@@ -1,7 +1,6 @@
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
-import {getServerSession} from "next-auth";
-
-import {redirect} from "next/navigation";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import UsernameForm from "@/components/forms/UsernameForm";
 import { Page } from "@/models/Page";
 import mongoose from "mongoose";
@@ -10,32 +9,33 @@ import PageSettingForm from "@/components/forms/PageSettingsForm";
 import PageButtonsForm from "@/components/forms/PageButtonsForm";
 import PageLinksForm from "@/components/forms/PageLinksForm";
 
-export default async function AccountPage({searchParams}) {
+export default async function AccountPage({ searchParams }) {
   const session = await getServerSession(authOptions);
   const desiredUsername = searchParams?.desiredUsername;
   if (!session) {
     return redirect('/');
   }
   mongoose.connect(process.env.MONGO_URI);
-  const page = await Page.findOne({owner: session?.user?.email});
-  const leanPage = cloneDeep(page.toJSON());
-  leanPage._id = leanPage._id.toString();
-  if(page) {
-    return  (
-      <div className="p-2 md:p-8 m-auto">
-       <PageSettingForm page={leanPage} user={session?.user}/>
-       <PageButtonsForm page={leanPage} user={session?.user} />
-       <PageLinksForm page={leanPage} user={session?.user} />
+  const page = await Page.findOne({ owner: session?.user?.email });
+  
+  if (!page) {
+    return (
+      <div className='max-w-7xl m-auto sm:p-8 p-4 '>
+        <h1 className="text-2xl font-semibold sm:text-3xl">Grab your Username</h1>
+        <p className="mt-2 sm:text-base text-sm">Choose your username...</p>
+        <UsernameForm desiredUsername={desiredUsername}/>
       </div>
-      
-    )
+    );
   }
 
+  const leanPage = cloneDeep(page.toJSON());
+  leanPage._id = leanPage._id.toString();
+  
   return (
-    <div className='max-w-7xl m-auto sm:p-8 p-4 '>
-      <h1 className="text-2xl font-semibold sm:text-3xl">Grab your Username</h1>
-      <p className="mt-2 sm:text-base text-sm">Choose your username...</p>
-      <UsernameForm desiredUsername={desiredUsername}/>
-   </div>
-  )
+    <div className="p-2 md:p-8 m-auto">
+      <PageSettingForm page={leanPage} user={session?.user}/>
+      <PageButtonsForm page={leanPage} user={session?.user} />
+      <PageLinksForm page={leanPage} user={session?.user} />
+    </div>
+  );
 }
--- src/app/(default)/VisualPage.js
+++ src/app/(default)/VisualPage.js
@@ -1,5 +1,6 @@
 
 import AnimationWrapper from '@/components/Animation';
+import HeroForm from '@/components/forms/HeroForm';
 import Link from 'next/link';
 import React from 'react'
 import { CiLink } from "react-icons/ci";
@@ -9,7 +10,6 @@ const VisualPage = () => {
     <div className='h-full  pt-12 pb-12 bg-normal-dark text-white'>
      <div className='max-w-7xl p-4 m-auto'>
       <div>
-        
         <div className='flex  mt-10 items-center justify-center'>
              <AnimationWrapper>
                 <h1 className='border flex  items-center gap-x-2 border-white/30  py-2 px-4 rounded-full'> <CiLink/>Welcome to Linkit! </h1>
@@ -24,12 +24,13 @@ const VisualPage = () => {
               consolidate your online presence and track views on your shared links in real-time and gaining 
               valuable insights into your reach and engagement.</p>
          <div>
-           <Link href="/login">
-             <button className='bg-white text-normal-dark mt-10 rounded-lg ng-white py-2 px-12'>Signup</button>
-           </Link>
         </div>
         </div>
         </AnimationWrapper>
+        <div className='flex items-center justify-center mt-10 mb-10'>
+           <HeroForm/>
+        </div>
+       
       </div>
      </div>
     </div>
